8acf27975e34d48ded8c504947458badb66641bf
Eon Pace <nicolaspace21@gmail.com>
2024-07-08 15:02:04 -0400

    code now allows for READ Me Generation Using Javascript

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,93 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateReadMe = ({ title, discription, installation, usage, test, license, github, email, credit}) =>
+`# ${title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Description
+- ${discription}   
 
-// TODO: Create a function to initialize app
-function init() {}
+## Table of contents
+- [Installation](#installation)
+- [License](#license)
+- [Usage](#usage)
+- [Test](#tests)
+- [Credits](#credits)
+    
 
-// Function call to initialize app
-init();
+## Installation
+- ${installation}
+
+## License
+- ${license}
+
+## Usage
+ - ${usage}
+
+## Test
+ - ${test}
+   
+## Credits
+ - ${credit, github, email}` 
+;
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What would you like the project title to be?',
+            name: 'title',
+        },
+
+        {
+            type: 'input',
+            message: 'What is the project discription?',
+            name: 'discription',
+        },
+
+        {
+            type: 'input',
+            message: 'What are the installation instructions (put n/a if not applicable)',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Provide information on how to use the program?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'How to test the program instruction',
+            name: 'test',
+        },
+        {
+            type: 'list',
+            message: 'What type of license does this program have?',
+            name: 'license',
+            choices: ['mit', 'CCO', 'bsd'],
+        },
+        {
+            type: 'input',
+            message: 'Who should be credited on this project?',
+            name: 'credit',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'github',
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+        
+    ])
+    .then((answers) => {
+        const readMeContent = generateReadMe(answers);
+
+        fs.writeFile('READMEResult.md', readMeContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md!')
+        );
+    });
